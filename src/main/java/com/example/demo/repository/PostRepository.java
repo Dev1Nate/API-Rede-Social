59d76a745b40cd58bf3e1628f9c8dae4d61a7add
davi <conserdavi@gmail.>
2025-08-04 09:55:59 -0300

    criacao dos repositorios

---
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import com.example.demo.entities.Post;
+import com.example.demo.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post,String> {
+}
