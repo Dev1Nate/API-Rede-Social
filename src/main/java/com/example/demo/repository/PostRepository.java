0aeb77e0107b4bfe04f109041245fd5ab5a1a4f9
davi <conserdavi@gmail.>
2025-08-05 18:25:39 -0300

    adicao de um query personalizada

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -3,10 +3,13 @@ package com.example.demo.repository;
 import com.example.demo.entities.Post;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
 
+    @Query("{ 'title': {$regex: ?0, $options:'i' } }")
+    List<Post> findbytittle(String title);
     List<Post> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
     }
 
     public List<Post> findbyTitle(String title){
-        return repository.findByTitleContainingIgnoreCase(title);
+        return repository.findbytittle(title);
     }
 }
